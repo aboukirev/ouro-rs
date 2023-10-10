54b8cc3c041a804ceb6bef0af187997d18cb6948
Alexei Boukirev <aboukirev@gmail.com>
2023-10-09 21:56:47 -0500

    Restructure for builder, add packetizer.

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ The big advantage of the projects like this is it requires serious dive into the
 I am picking up some ideas from similar and not so similar projects on the github and improvise if I do not find something I need.  Perhaps others can benefit from reading my code.  Let's see where it goes.
 
 In the Go version of the code I implmented RTP (not very safe, assumes server sends correct packets), RTCP, RTSP, rudimentary SDP, RTSP over HTTP (Apple extensions), some basics of h264 content parsing (NALU handling, Annex B).  I'll follow the same order here but try to be more thorough implementing validation wherever possible.  In the past I got stuck at handling multiple NALUs per packet and combining packets.  Just need more time to see how it appears from cameras to have a set of data for practice.
+
+## TODO
+- [ ] Implement RTP packet bulder (packetizer).
+- [ ] Implement validations while building RTP packet to ensure permanent correctness.
+- [ ] Implement BEDE extension parser and builder.
+- [ ] Implement sequence number generator: unpredictable initial value, increment, multiplexing support.
+- [ ] Implement RTCP parser.
+- [ ] Implement RTCP builder.
+- [ ] Implement simple SDP parser (this is one area I'd like to skimp on until later time).
+- [ ] Implement digest algorithm for RTSP authentication.
+- [ ] Implement message protocol for RTSP.
+- [ ] Implement state machine for RTSP.
+- [ ] Implement RTSP over HTTP.
--- src/rtp.rs
+++ src/rtp.rs
@@ -1,10 +1,11 @@
 use std::fmt;
+use rand::Rng;
 
 #[derive(Debug)]
 pub enum RtpError {
     InvalidLen(usize),
     InvalidVersion(u8),
-    InvalidCSRCCount(usize),
+    InvalidCSRCCount(u8),
     MissingExtension,
     InvalidExtensionLength(usize),
     InvalidPadding(usize),
@@ -12,7 +13,15 @@ pub enum RtpError {
 
 #[derive(Clone, Eq, PartialEq)]
 pub struct RtpPacket<'a> {
-    buf: &'a [u8],
+    cc: u8,
+    payload_type: u8,
+    seq_number: u16,
+    timestamp: u32,
+    ssrc: u32,
+    csrc: [u32; 15],
+    extension: Option<RtpExtension<'a>>,
+    payload: &'a [u8],
+    mark: bool,
 }
 
 #[derive(Clone, Eq, PartialEq)]
@@ -27,6 +36,27 @@ impl<'a> RtpPacket<'a> {
     // Fixed RTP protocol version.
     const RTP_VERSION: u8 = 2;
 
+    pub fn new(
+        mark: bool,
+        payload_type: u8,
+        seq_number: u16,
+        timestamp: u32,
+        ssrc: u32,
+        payload: &'a [u8],
+    ) -> RtpPacket<'_> {
+        RtpPacket { 
+            cc: 0u8, 
+            payload_type: payload_type, 
+            seq_number: seq_number, 
+            timestamp: timestamp, 
+            ssrc: ssrc, 
+            csrc: [0u32; 15], 
+            extension: None, 
+            payload: payload, 
+            mark: mark, 
+        }
+    }
+
     pub fn from_slice(slice: &'a [u8]) -> Result<RtpPacket<'_>, RtpError> {
         let slice_len = slice.len();
         if slice_len < RtpPacket::HEADER_SIZE {
@@ -36,121 +66,66 @@ impl<'a> RtpPacket<'a> {
         if version != RtpPacket::RTP_VERSION {
             return Err(RtpError::InvalidVersion(version))
         }
-        let packet = RtpPacket { buf: slice };
+        let cc = slice[0] & 0x0F;
+        let mut csrc = [0u32; 15];
+        let pad_flag = (slice[0] & 0x20) >> 5;  // 0 or 1
+        let mut off = RtpPacket::HEADER_SIZE + (cc as usize) * 4;
+
+        for index in 0..cc as usize {
+            let csrc_off = off + (cc as usize) * 4;
+            csrc[index] = u32::from_be_bytes([slice[csrc_off], slice[csrc_off + 1], slice[csrc_off + 2], slice[csrc_off + 3]])
+        }
+
         // The following additional validation checks are declared as complex and not always possible in the RFC 1889.
-        // Perform validation here so that subsequent parsing can rely on offsets and indexes not going out of bounds.
-        let ext_off = packet.extension_off();
-        if ext_off > packet.buf.len() {
-            return Err(RtpError::InvalidCSRCCount(packet.cc()))
+        if off > slice_len {
+            return Err(RtpError::InvalidCSRCCount(cc))
         }
-        if (packet.buf[0] & 0x10) != 0 && (ext_off + 4) > packet.buf.len() {
-            return Err(RtpError::MissingExtension)
+        let mut extension: Option<RtpExtension> = None;
+        if (slice[0] & 0x10) != 0 {
+            if (off + 4) > slice_len {
+                return Err(RtpError::MissingExtension)
+            }
+            let ext_len = (u16::from_be_bytes([slice[off + 2], slice[off + 3]]) as usize) * 4 + 4;
+            if (off + ext_len) > slice_len {
+                return Err(RtpError::InvalidExtensionLength(ext_len))
+            }
+            extension = Some(RtpExtension {
+                head: u16::from_be_bytes([slice[off], slice[off + 1]]),
+                data: &slice[(off + 4)..(off + ext_len)],
+            });
+            off += ext_len;
         }
-        let ext_len = packet.extension_len();
-        if (ext_off + ext_len) > packet.buf.len() {
-            return Err(RtpError::InvalidExtensionLength(ext_len))
-        }
-        let pad_len = packet.padding();
-        if (ext_off + ext_len + pad_len) > packet.buf.len() {
+        let pad_len = (slice[slice_len - 1] * pad_flag) as usize;
+        if (off + pad_len) > slice_len {
             return Err(RtpError::InvalidPadding(pad_len))
         }
-        Ok(packet)
-    }
 
-    #[inline]
-    pub fn cc(&self) -> usize {
-        (self.buf[0] as usize) & 0x0F
-    } 
-
-    #[inline]
-    pub fn mark(&self) -> bool {
-        (self.buf[1] & 0x80) != 0
-    } 
-
-    #[inline]
-    pub fn payload_type(&self) -> u8 {
-        self.buf[1] & 0x7F
-    } 
-    
-    #[inline]
-    pub fn seq_number(&self) -> u16 {
-        u16::from_be_bytes([self.buf[2], self.buf[3]])
-    }
-
-    #[inline]
-    pub fn timestamp(&self) -> u32 {
-        u32::from_be_bytes([self.buf[4], self.buf[5], self.buf[6], self.buf[7]])
-    }
-
-    #[inline]
-    pub fn ssrc(&self) -> u32 {
-        u32::from_be_bytes([self.buf[8], self.buf[9], self.buf[10], self.buf[11]])
-    }
-
-    #[inline]
-    pub fn extension_off(&self) -> usize {
-        // Static/minimal header size plus SSRC and CSRC elements.
-        RtpPacket::HEADER_SIZE + ((self.buf[0] & 0x0F) as usize) * 4
-    }
-
-    pub fn csrc(&self) -> Vec<u32> {
-        self.buf[RtpPacket::HEADER_SIZE..]
-            .chunks_exact(4)
-            .take(self.cc())
-            .map(|c| u32::from_be_bytes([c[0], c[1], c[2], c[3]]))
-            .collect::<Vec<_>>()
-    }
-
-    // Extension length in bytes counting the extension header.
-    pub fn extension_len(&self) -> usize {
-        if (self.buf[0] & 0x10) == 0 {
-            return 0
-        }
-        let off = self.extension_off();
-        (u16::from_be_bytes([self.buf[off + 2], self.buf[off + 3]]) as usize) * 4 + 4
-    }
-
-    pub fn extension(&self) -> Option<RtpExtension> {
-        let ext_len = self.extension_len();
-        if ext_len == 0 {
-            return None
-        }
-        let off = self.extension_off();
-        let ext = RtpExtension {
-            head: u16::from_be_bytes([self.buf[off], self.buf[off + 1]]),
-            data: &self.buf[(off + 4)..(off + ext_len)],
-        };
-        Some(ext)
-    }
-
-    pub fn padding(&self) -> usize {
-        let total_len = self.buf.len();
-        if (self.buf[0] & 0x20) != 0 {
-            return self.buf[total_len - 1] as usize;
-        }
-        0
-    }
-
-    pub fn payload(&self) -> &[u8] {
-        let total_len = self.buf.len();
-        let pad_len = self.padding();
-        let off: usize = RtpPacket::HEADER_SIZE + self.cc() * 4 + self.extension_len();
-        &self.buf[off..(total_len - pad_len)]
+        Ok(RtpPacket { 
+            cc: cc, 
+            payload_type: slice[1] & 0x7F, 
+            seq_number: u16::from_be_bytes([slice[2], slice[3]]), 
+            timestamp: u32::from_be_bytes([slice[4], slice[5], slice[6], slice[7]]), 
+            ssrc: u32::from_be_bytes([slice[8], slice[9], slice[10], slice[11]]), 
+            csrc: csrc, 
+            extension: extension, 
+            payload: &slice[off..(slice_len - pad_len)], 
+            mark: (slice[1] & 0x80) != 0, 
+        })
     }
 }
 
 impl<'a> fmt::Debug for RtpPacket<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         f.debug_struct("RtpPacket")
-            .field("cc", &self.cc())
-            .field("mark", &self.mark())
-            .field("payload_type", &self.payload_type())
-            .field("seq_number", &self.seq_number())
-            .field("timestamp", &self.timestamp())
-            .field("ssrc", &self.ssrc())
-            .field("csrc", &self.csrc())
-            .field("extension", &self.extension())
-            .field("payload_len", &self.payload().len())
+            .field("cc", &self.cc)
+            .field("mark", &self.mark)
+            .field("payload_type", &self.payload_type)
+            .field("seq_number", &self.seq_number)
+            .field("timestamp", &self.timestamp)
+            .field("ssrc", &self.ssrc)
+            .field("csrc", &self.csrc)
+            .field("extension", &self.extension)
+            .field("payload_len", &self.payload.len())
             .finish()
     }
 }
@@ -164,6 +139,56 @@ impl<'a> fmt::Debug for RtpExtension<'a> {
             .finish()
     }
 }
+
+pub struct RtpPacketizer {
+    mtu: usize,
+    payload_type: u8,
+    seq_number: u16,
+    timestamp: u32,
+    ssrc: u32,
+}
+
+impl RtpPacketizer {
+    pub fn new(
+        mtu: usize,
+        payload_type: u8,
+        ssrc: u32,
+    ) -> Self {
+        let mut rng = rand::thread_rng();
+        RtpPacketizer { 
+            mtu: mtu, 
+            payload_type: payload_type, 
+            seq_number: rng.gen::<u16>(), 
+            timestamp: rng.gen::<u32>(), 
+            ssrc: ssrc, 
+        }
+    }
+
+    pub fn packetize<'a>(&'a mut self, payload: &'a [u8], frames: u32) -> Vec<RtpPacket<'_>> {
+        self.timestamp = self.timestamp.wrapping_add(frames);
+        // At this point assume just a standard fixed header, no csrc, no extension.  Only the last chunk may require padding.
+        let chunk_size = self.mtu - RtpPacket::HEADER_SIZE;
+        let chunk_count = payload.len().div_ceil(chunk_size);
+        let mut packets = Vec::<RtpPacket>::with_capacity(chunk_count);
+
+        for index in 0..chunk_count {
+            self.seq_number = self.seq_number.wrapping_add(1);
+            let off = chunk_size * index;
+            let span = usize::min(chunk_size, payload.len() - off);
+
+            packets.push(RtpPacket::new(
+                index == (chunk_count - 1),
+                self.payload_type,
+                self.seq_number,  
+                self.timestamp, 
+                self.ssrc,
+                &payload[off..off + span], 
+            ))
+        }
+        packets
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -208,14 +233,14 @@ mod tests {
             0x98, 0x36, 0xbe, 0x88, 0x9e,
         ];
         let packet = RtpPacket::from_slice(&data).unwrap();
-        assert!(packet.extension().is_some());
-        assert_eq!(0, packet.cc());
-        assert!(packet.mark());
-        assert_eq!(96, packet.payload_type());
-        assert_eq!(27023, packet.seq_number());
-        assert_eq!(3653407706, packet.timestamp());
-        assert_eq!(476325762, packet.ssrc());
-        assert_eq!(5, packet.payload().len());
+        assert!(packet.extension.is_some());
+        assert_eq!(0, packet.cc);
+        assert!(packet.mark);
+        assert_eq!(96, packet.payload_type);
+        assert_eq!(27023, packet.seq_number);
+        assert_eq!(3653407706, packet.timestamp);
+        assert_eq!(476325762, packet.ssrc);
+        assert_eq!(5, packet.payload.len());
     }
 
     #[test]
@@ -226,14 +251,14 @@ mod tests {
             0x98, 0x36, 0xbe, 0x88, 0x04,
         ];
         let packet = RtpPacket::from_slice(&data).unwrap();
-        assert!(packet.extension().is_some());
-        assert_eq!(0, packet.cc());
-        assert!(packet.mark());
-        assert_eq!(96, packet.payload_type());
-        assert_eq!(27023, packet.seq_number());
-        assert_eq!(3653407706, packet.timestamp());
-        assert_eq!(476325762, packet.ssrc());
-        assert_eq!(1, packet.payload().len());
+        assert!(packet.extension.is_some());
+        assert_eq!(0, packet.cc);
+        assert!(packet.mark);
+        assert_eq!(96, packet.payload_type);
+        assert_eq!(27023, packet.seq_number);
+        assert_eq!(3653407706, packet.timestamp);
+        assert_eq!(476325762, packet.ssrc);
+        assert_eq!(1, packet.payload.len());
     }
 
     #[test]
@@ -244,14 +269,14 @@ mod tests {
             0x98, 0x36, 0xbe, 0x88, 0x05,
         ];
         let packet = RtpPacket::from_slice(&data).unwrap();
-        assert!(packet.extension().is_some());
-        assert_eq!(0, packet.cc());
-        assert!(packet.mark());
-        assert_eq!(96, packet.payload_type());
-        assert_eq!(27023, packet.seq_number());
-        assert_eq!(3653407706, packet.timestamp());
-        assert_eq!(476325762, packet.ssrc());
-        assert_eq!(0, packet.payload().len());
+        assert!(packet.extension.is_some());
+        assert_eq!(0, packet.cc);
+        assert!(packet.mark);
+        assert_eq!(96, packet.payload_type);
+        assert_eq!(27023, packet.seq_number);
+        assert_eq!(3653407706, packet.timestamp);
+        assert_eq!(476325762, packet.ssrc);
+        assert_eq!(0, packet.payload.len());
     }
 
     #[test]
@@ -273,14 +298,14 @@ mod tests {
             0x00, 0x00, 0x00, 0x00, 0x05,
         ];
         let packet = RtpPacket::from_slice(&data).unwrap();
-        assert!(packet.extension().is_some());
-        assert_eq!(0, packet.cc());
-        assert!(packet.mark());
-        assert_eq!(96, packet.payload_type());
-        assert_eq!(27023, packet.seq_number());
-        assert_eq!(3653407706, packet.timestamp());
-        assert_eq!(476325762, packet.ssrc());
-        assert_eq!(5, packet.payload().len());
+        assert!(packet.extension.is_some());
+        assert_eq!(0, packet.cc);
+        assert!(packet.mark);
+        assert_eq!(96, packet.payload_type);
+        assert_eq!(27023, packet.seq_number);
+        assert_eq!(3653407706, packet.timestamp);
+        assert_eq!(476325762, packet.ssrc);
+        assert_eq!(5, packet.payload.len());
     }
 
     #[test]
@@ -291,9 +316,24 @@ mod tests {
 		    0x98, 0x36, 0xbe, 0x88, 0x9e,
         ];
         let packet = RtpPacket::from_slice(&data).unwrap();
-        assert!(packet.extension().is_some());
-        if let Some(extension) = packet.extension() {
+        assert!(packet.extension.is_some());
+        if let Some(extension) = packet.extension {
             assert_eq!(4, extension.data.len());
         }
+    }
+
+    #[test]
+    fn packetize_two_packets() {
+        let data = [0u8; 128];
+        let mut packetizer = RtpPacketizer::new(100, 98, 0x1234ABCD);
+        let packets = packetizer.packetize(&data, 2000);
+        assert_eq!(2, packets.len());
+        let packet = packets.get(1).unwrap();
+        assert!(packet.extension.is_none());
+        assert_eq!(0, packet.cc);
+        assert!(packet.mark);
+        assert_eq!(98, packet.payload_type);
+        assert_eq!(0x1234ABCD, packet.ssrc);
+        assert_eq!(40, packet.payload.len());
     }
 }
